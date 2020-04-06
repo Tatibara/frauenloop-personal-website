2e1d2dc4fae6bc760f7eab5b11eb8b59e635b734
Tatiana Baradari <8246564+Tatibara@users.noreply.github.com>
2020-04-06 09:37:34 +0200

    added createdAt, updatedAt

--- week_10/03_homework_sample_solution/src/components/BlogEntry.jsx
+++ week_10/03_homework_sample_solution/src/components/BlogEntry.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const BlogEntry = ({ blogEntry }) => (
   <>
-    <h4>{blogEntry.title}</h4>
-    <p>{blogEntry.body}</p>
+    <h4>{`Title: ${blogEntry.title}`}</h4>
+    <p>{`Summary: ${blogEntry.summary}`}</p>
+    <p>{`Body: ${blogEntry.body}`}</p>
+    <p>{`Created at: ${new Date(blogEntry.createdAt).toLocaleDateString('en-US')}`}</p>
+    <p>{blogEntry.updatedAt && `Updated at: ${new Date(blogEntry.updatedAt).toLocaleDateString('en-US')}`}</p>
   </>
 );
 
